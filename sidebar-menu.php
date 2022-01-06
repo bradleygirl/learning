714170b7079b88d6c1f8f302b7afa8a49b8de403
Mary B <bradleymary@gmail.com>
2022-01-06 16:44:14 -0500

    Added dummy php file to work on

---
+++ sidebar-menu.php
@@ -0,0 +1,117 @@
+<php?
+
+<div id="woocommerce_product_categories-5" class="widget woocommerce widget_product_categories"><select name="product_cat" id="product_cat" class="dropdown_product_cat select2-hidden-accessible" tabindex="-1" aria-hidden="true">
+	<option value="">Select a category</option>
+	<option class="level-0" value="surgical-instruments">Plastic Surgery Instruments</option>
+	<option class="level-1" value="procedure-sets">&nbsp;&nbsp;&nbsp;Procedure Sets (Trays)</option>
+	<option class="level-1" value="scissors">&nbsp;&nbsp;&nbsp;Scissors</option>
+	<option class="level-1" value="needle-holders">&nbsp;&nbsp;&nbsp;Needle Holders</option>
+	<option class="level-1" value="thumb-forceps">&nbsp;&nbsp;&nbsp;Thumb Forceps</option>
+	<option class="level-1" value="rasps-morselizers">&nbsp;&nbsp;&nbsp;Rasps &amp; Morselizers</option>
+	<option class="level-1" value="osteotomes-mallets">&nbsp;&nbsp;&nbsp;Osteotomes &amp; Mallets</option>
+	<option class="level-1" value="elevators-spec">&nbsp;&nbsp;&nbsp;Elevators &amp; Speculums</option>
+	<option class="level-1" value="hooks-measuring">&nbsp;&nbsp;&nbsp;Hooks &amp; Measuring</option>
+	<option class="level-1" value="retractors-diss">&nbsp;&nbsp;&nbsp;Retractors &amp; Dissectors</option>
+	<option class="level-1" value="ring-forceps">&nbsp;&nbsp;&nbsp;Ring Forceps &amp; Rongeurs</option>
+	<option class="level-1" value="markers-knives">&nbsp;&nbsp;&nbsp;Markers &amp; Knives</option>
+	<option class="level-1" value="electrosurgical">&nbsp;&nbsp;&nbsp;Electrosurgical</option>
+	<option class="level-1" value="misc">&nbsp;&nbsp;&nbsp;Miscellaneous</option>
+	<option class="level-1" value="instrument-care">&nbsp;&nbsp;&nbsp;Instrument Care</option>
+	<option class="level-1" value="endo-instruments">&nbsp;&nbsp;&nbsp;Endoscopic Plastic Instruments</option>
+	<option class="level-1" value="headlights-light-sources">&nbsp;&nbsp;&nbsp;Headlights &amp; Light Sources</option>
+	<option class="level-0" value="micro-surgery-instruments">Micro Surgery Instruments</option>
+	<option class="level-1" value="micro-surgery-instruments-clamps">&nbsp;&nbsp;&nbsp;Clamps</option>
+	<option class="level-1" value="micro-forceps">&nbsp;&nbsp;&nbsp;Micro Forceps</option>
+	<option class="level-1" value="micro-needle-holders">&nbsp;&nbsp;&nbsp;Micro Needle Holders</option>
+	<option class="level-1" value="scissors-micro">&nbsp;&nbsp;&nbsp;Micro Scissors</option>
+	<option class="level-0" value="liposuction">Vitruvian™ Liposuction</option>
+	<option class="level-1" value="lipo-equipment">&nbsp;&nbsp;&nbsp;Liposuction Equipment</option>
+	<option class="level-1" value="lipo-cannulas">&nbsp;&nbsp;&nbsp;Lipo Cannulas</option>
+	<option class="level-1" value="fat-collection-canisters">&nbsp;&nbsp;&nbsp;Fat Collection Canisters</option>
+	<option class="level-1" value="lipo-accessories">&nbsp;&nbsp;&nbsp;Liposuction Accessories</option>
+	<option class="level-1" value="lipo-disposables">&nbsp;&nbsp;&nbsp;Liposuction Disposables</option>
+</select><span class="select2 select2-container select2-container--default select2-container--focus" dir="ltr" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--single" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-product_cat-container" role="combobox"><span class="select2-selection__rendered" id="select2-product_cat-container" role="textbox" aria-readonly="true"><span class="select2-selection__placeholder">Select a category</span></span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
+</div>
+
+======
+
+<!-- CSS
+#secondary .custom-html-widget > select, #secondary .widget_product_categories > select {width:98.95%;padding: .25em;background-color: #f0f9fb;border: 1px solid #b6c2c3;border-radius: .3em;}
+#secondary .widget_woocommerce .widget-title, #secondary .custom-html-widget .widget-title {font-size: 1.15em; padding: 0 0 .2em .3em; margin-bottom: 0;font-weight: 500; border-bottom: 0;}
+-->
+
+<option value="">Select Instrument Set</option>
+<select id="choose_procedure_sets" class="dropdown_product_cat select2-hidden-accessible">
+<option class="level-0" value="0">Instrument Sets/Trays:</option>
+<option class="level-0" value="">Blepharoplasty Sets</option>
+<option class="level-1" value="kaye-blepharoplasty-procedure-set" data-link="kaye-blepharoplasty-procedure-set">&nbsp;&nbsp;&nbsp;Kaye Blepharoplasty Set</option>
+<option class="level-1" value="seiff-blepharoplasty-set" data-link="seiff-blepharoplasty-set/">&nbsp;&nbsp;&nbsp;Seiff Blepharoplasty Set</option>
+<option class="level-0" value="">Breast Surgery Sets</option>
+<option class="level-1" value="bostwick-breast-set" data-link="bostwick-breast-set">&nbsp;&nbsp;&nbsp;Bostwick Breast Procedure Set</option>
+<option class="level-1" value="cox-nipple-sparing-mastectomy-set" data-link="cox-nipple-sparing-mastectomy-set">&nbsp;&nbsp;&nbsp;Cox Nipple-Sparing Mastectomy Set</option>
+<option class="level-1" value="tebbetts-breast-set" data-link="tebbetts-breast-set">&nbsp;&nbsp;&nbsp;Tebbetts Breast Set</option>
+<option class="level-0" value="">Endoscopic Sets</option>
+<option class="level-1" value="daniel-endoscopic-forehead-set" data-link="daniel-endoscopic-forehead-set">&nbsp;&nbsp;&nbsp;Daniel Endoscopic Forehead Set</option>
+<option class="level-1" value="guyuron-endoscopic-migraine-set" data-link="guyuron-endoscopic-migraine-set">&nbsp;&nbsp;&nbsp;Guyuron Endoscopic Migraine Set</option>
+<option class="level-1" value="ramirez-ergo-style-endoscopic-set" data-link="ramirez-ergo-style-endoscopic-set">&nbsp;&nbsp;&nbsp;Ramirez Ergo-Style Endoscopic Set</option>
+<option class="level-0" value="">Face-Lift Sets</option>
+<option class="level-1" value="aston-face-lift-procedure-set" data-link="aston-face-lift-procedure-set">&nbsp;&nbsp;&nbsp;Aston Face-lift Procedure Set</option>
+<option class="level-1" value="gorney-face-lift-procedure-set" data-link="gorney-face-lift-procedure-set">&nbsp;&nbsp;&nbsp;Gorney Face-lift Procedure Set</option>
+<option class="level-0" value="">General</option>
+<option class="level-1" value="janis-abdominal-wall-set" data-link="janis-abdominal-wall-set">&nbsp;&nbsp;&nbsp;Janis Abdominal Wall Set</option>
+<option class="level-1" value="janis-comprehensive-plastic-surgery-set" data-link="janis-comprehensive-plastic-surgery-set">&nbsp;&nbsp;&nbsp;Janis Comprehensive Plastic Surgery Set</option>
+<option class="level-0" value="">Lipoplasty Sets</option>
+<option class="level-1" value="autologous-fat-transfer-set" data-link="autologous-fat-transfer-set">&nbsp;&nbsp;&nbsp;Autologous Fat Transfer Set</option>
+<option class="level-1" value="tumescent-infiltration-set" data-link="tumescent-infiltration-set">&nbsp;&nbsp;&nbsp;Tumescent Infiltration Set</option>
+<option class="level-0" value="">Rhinoplasty Sets</option>
+<option class="level-1" value="gruber-rhinoplasty-procedure-set" data-link="gruber-rhinoplasty-procedure-set">&nbsp;&nbsp;&nbsp;Gruber Rhinoplasty Procedure Set</option>
+<option class="level-1" value="gunter-rhinoplasty-procedure-set" data-link="gunter-rhinoplasty-procedure-set">&nbsp;&nbsp;&nbsp;Gunter Rhinoplasty Procedure Set</option>
+<option class="level-1" value="peck-rhinoplasty-procedure-set" data-link="peck-rhinoplasty-procedure-set">&nbsp;&nbsp;&nbsp;Peck Rhinoplasty Procedure Set</option>
+<option class="level-1" value="sheen-rhinoplasty-procedure-set" data-link="sheen-rhinoplasty-procedure-set">&nbsp;&nbsp;&nbsp;Sheen Rhinoplasty Procedure Set</option>
+<option class="level-1" value="silver-rhinoplasty-set" data-link="silver-rhinoplasty-set">&nbsp;&nbsp;&nbsp;Silver Rhinoplasty Procedure Set</option>
+<option class="level-1" value="tebbetts-rhinoplasty-procedure-set" data-link="tebbetts-rhinoplasty-procedure-set">&nbsp;&nbsp;&nbsp;Tebbetts Rhinoplasty Procedure Set</option>
+</select><span class="select2 select2-container select2-container--default select2-container--focus" dir="ltr" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--single" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-product_cat-container" role="combobox"><span class="select2-selection__rendered" id="select2-product_cat-container" role="textbox" aria-readonly="true"><span class="select2-selection__placeholder">Select Instrument Set</span></span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
+
+<script type='text/javascript'>
+    jQuery(function($){
+            var procedure_dropdown = $(".dropdown_procedure_sets");
+            function onProcedureSetChange() {
+                if ( procedure_dropdown.val() !=='' ) {
+                    location.href = "https://www.blackandblacksurgical.com/product/" +procedure_dropdown.val();
+                }
+            }
+            procedure_dropdown.change( onProcedureSetChange );
+        });
+</script>
+
+<span style="font-weight:600;margin-bottom:.25em;float:left;padding-top:1em;">Select a Surgical Procedure:</span>
+<select id="choose_procedure_tag" class="dropdown_procedure_tags">
+<option class="level-0" value="0">Surgical Procedures:</option>
+<option class="level-1"  value="abdominoplasty" data-link="abdominoplasty">&nbsp;&nbsp;&nbsp;Abdominoplasty</option>
+<option class="level-1"  value="blepharoplasty" data-link="blepharoplasty">&nbsp;&nbsp;&nbsp;Blepharoplasty</option>
+<option class="level-1"  value="breast-surgery" data-link="breast-surgery">&nbsp;&nbsp;&nbsp;Breast Surgery</option>
+<option class="level-1"  value="electrosurgery" data-link="electrosurgery">&nbsp;&nbsp;&nbsp;Electrosurgery</option>
+<option class="level-1"  value="endoplastic-surg" data-link="endoplastic-surg">&nbsp;&nbsp;&nbsp;Endoplastic</option>
+<option class="level-1"  value="face-lift" data-link="face-lift">&nbsp;&nbsp;&nbsp;Face Lift</option>
+<option class="level-1"  value="hand-surgery" data-link="hand-surgery">&nbsp;&nbsp;&nbsp;Hand Surgery</option>
+<option class="level-1"  value="lipoplasty" data-link="lipoplasty">&nbsp;&nbsp;&nbsp;Lipoplasty</option>
+<option class="level-1"  value="oral-surgery" data-link="oral-surgery">&nbsp;&nbsp;&nbsp;Oral Surgery</option>
+<option class="level-1"  value="rhinoplasty" data-link="rhinoplasty">&nbsp;&nbsp;&nbsp;Rhinoplasty</option>
+<option class="level-1"  value="fat-grafting" data-link="fat-grafting">&nbsp;&nbsp;&nbsp;Fat Grafting</option>
+<!--<option class="level-1"  value="liposuction" data-link="liposuction">&nbsp;&nbsp;&nbsp;Liposuction</option>-->
+
+</select>
+
+<script type='text/javascript'>
+    jQuery(function($){
+            var proceduretags_dropdown = $(".dropdown_procedure_tags");
+            function onProcedureSetChange() {
+                if ( proceduretags_dropdown.val() !=='' ) {
+                    location.href = "https://www.blackandblacksurgical.com/product-tag/" +proceduretags_dropdown.val();
+                }
+            }
+            proceduretags_dropdown.change( onProcedureSetChange );
+        });
+</script>
+
+?>
